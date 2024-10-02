eba3d6e34c1c0f2fd7e4f63162dde5803ee10735
Ido Slonimsky <41922392+IdoKendo@users.noreply.github.com>
2024-10-02 09:18:12 +0300

    chore: use ruff format instead of black (#8)

--- robyn_rate_limits/protocols.py
+++ robyn_rate_limits/protocols.py
@@ -2,5 +2,8 @@ from typing import Protocol
 
 
 class LimitStore(Protocol):
-    def get_calls_count(self, limit_key: str, current_timestamp: int) -> int:
-        ...  # pragma: no cover
+    def get_calls_count(
+        self,
+        limit_key: str,
+        current_timestamp: int,
+    ) -> int: ...  # pragma: no cover
